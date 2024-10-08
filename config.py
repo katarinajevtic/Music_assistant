b66a3e1c9b29a2eb11b5393e84fb1f9ae282dfbd
Katarina Jevtic <60469171+katarinajevtic@users.noreply.github.com>
2024-10-08 14:34:01 +0200

    update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 import streamlit as st
-SPOTIFY_CLIENT_ID = st.secrets["SPOTIFY_CLIENT_D"] # Replace with your RapidAPI key'
+SPOTIFY_CLIENT_ID = st.secrets["SPOTIFY_CLIENT_ID"] # Replace with your RapidAPI key'
 SPOTIFY_CLIENT_SECRET = st.secrets["SPOTIFY_CLIENT_SECRET"]
